9859d001ced5901d84f64de786c31aa7c79a6bd2
Daniel Bälz <me@dbaelz.de>
2016-12-07 22:08:21 +0100

    Improve read in of the config file and add basic validation

--- seluks.sh
+++ seluks.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
-source seluks.config
+configFile="$(pwd)/seluks.config"
 
-if [ "$1" == "open" ]; then
-	echo "Open container $container"
+if [ ! -e $configFile ]
+then
+	printf "ERROR: $configFile not found\n"
+	exit -1
+else
+	source $configFile
+fi
+
+if [ "$1" == "validate" ]; then
+	printf "INFO: Validate $configFile\n"
+
+	noErrorCount=0
+	[[ -z "$container" ]] && printf "ERROR: Missing variable 'container'\n" || ((noErrorCount++))
+	[[ -z "$mapper" ]] && printf "ERROR: Missing variable 'mapper'\n" || ((noErrorCount++))
+	[[ -z "$mountpoint" ]] && printf "ERROR: Missing variable 'mountpoint'\n" || ((noErrorCount++))
+	[[ -z "$filesystem" ]] && printf "ERROR: Missing variable 'filesystem'\n" || ((noErrorCount++))
+
+	[[ $noErrorCount -eq 4  ]] && printf "\nSUCCESS: No missing variables\n"
+
+elif [ "$1" == "open" ]; then
+	printf "INFO: Open container $container\n"
 	loopDevice=`sudo losetup -f`
 
 	if [ "`losetup -a | grep -c "$container"`" != "0" ]; then
-	        echo "Container already in use!"
-	        exit
+	        printf "\nERROR: Container $container already in use\n"
+	        exit -1
 	fi
 
 	sudo /sbin/losetup $loopDevice $container
 	sudo /sbin/cryptsetup luksOpen $loopDevice $mapper
 	sudo /bin/mount -t $filesystem /dev/mapper/$mapper $mountpoint
+	printf "\nSUCCESS: Opened container $container\n"
+
 elif [ "$1" == "close" ]; then
-	echo "Close container $container"
+	printf "INFO: Close container $container\n"
 	loopDevice=`sudo losetup -a | grep "$container" | sed "s/: .*//"`
 
 	if [ "`sudo losetup -a | grep -c "$container"`" != "1" ]; then
-		echo "Container not in use!"
-		exit
+		printf "\nERROR: Container $container not in use\n"
+		exit -1
 	fi
 
 	sudo /bin/umount $mountpoint
 	sudo /sbin/cryptsetup luksClose $mapper
 	sudo /sbin/losetup -d $loopDevice
+	printf "\nSUCCESS: Close container $container\n"
+
 else
-	echo "The mode is required as parameter. Allowed values: 'open' or 'close'"
+	printf "ERROR: Mode parameter is required. Allowed values: 'validate', 'open' or 'close'\n"
 fi
